a08cab7cea03bc9896ff2059e4e6bc712700ff7e
xujb1987 <xujb1987@971713a7-01c5-a0ac-fab6-219b94815afc>
2011-05-16 13:56:52 +0000

    Add support for Google Translate API

---
+++ Mashup/src/act/mashup/module/GoogleTranslate.java
@@ -0,0 +1,20 @@
+package act.mashup.module;
+
+import com.google.api.client.googleapis.json.GoogleApi;
+
+public class GoogleTranslate extends AbstractListModule {
+	
+
+	@Override
+	protected void Prepare() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void Execute() throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
